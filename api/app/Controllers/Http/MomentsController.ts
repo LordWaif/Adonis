07d77d5b2e374103d53555f3aa3ab84c34a89ada
LordWaif <jassonjcs11@gmail.com>
2022-12-08 11:34:22 -0300

    Adonis inicio

---
+++ api/app/Controllers/Http/MomentsController.ts
@@ -0,0 +1,35 @@
+import {v4 as uuidv4 } from 'uuid'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Application from '@ioc:Adonis/Core/Application'
+import Moment from 'App/Models/Moment'
+
+export default class MomentsController {
+    private validationOptions = {
+        type: ['image'],
+        size : '2mb',
+    }
+    public async store({request,response}: HttpContextContract){
+        const body = request.body()
+        const image = request.file('image',this.validationOptions)
+
+        if(image){
+            const imageName = `${uuidv4}.${image.extname}`
+
+            await image.move(Application.tmpPath('uploads'),{
+                name: imageName
+            })
+
+            body.image = imageName
+        }
+
+        const moment = await Moment.create(body)
+
+        response.status(201)
+
+        return {
+            message: 'Moment create',
+            data: moment,
+        }
+    }
+
+}
---
+++ script.sh
@@ -0,0 +1,8 @@
+npm i @adonisjs/lucid
+node ace configure @adonisjs/lucid
+node ace migration:run
+node ace
+node ace migration:fresh
+node ace make:controller Moment
+node ace list:routes
+npm i uuid
